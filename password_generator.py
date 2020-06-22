6159954d829108ce03498012cd7dc060465688f5
shreeyasethi <67246350+shreeyasethi@users.noreply.github.com>
2020-06-21 20:28:05 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,2 @@
-print(" ")
+print("Welcome! Today we will create a password for you to keep your accounts safe.")
+pickcharacters=input("How many characters long do you want you password to be ? (2-8)")
