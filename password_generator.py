615f097abe7beb018a2d14bdd36daa0fdca59558
shreeyasethi <67246350+shreeyasethi@users.noreply.github.com>
2020-06-22 12:37:40 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,18 @@
 import random
-letterlist = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-numberlist = ["1","2","3","4","5","6","7","8","9","0"]
-signslist = ["!","@","#","$","%","^","&","*","+","="] 
+
+characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*+="
 
 print("Welcome! Today we will create a password for you to keep your accounts safe.")
-pickcharacters=input("How many characters long do you want you password to be ? (2-8)")
-if pickcharacters=="2":
+print("\n")
+pickcharacters=int(input("How many characters long do you want you password to be?"))
+print ("Here is your new password:")
+print("\n")
+
+password = ""
+for x in range(pickcharacters): 
+  password = password + random.choice(characters)
+
+  print(password)
+
 
   
