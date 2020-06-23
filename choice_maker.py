7bbbb18d1c5e2d92d69486eaa4449adbc4e9ee78
shreeyasethi <67246350+shreeyasethi@users.noreply.github.com>
2020-06-22 23:48:16 -0400

    Update choice_maker.py

--- choice_maker.py
+++ choice_maker.py
@@ -1,1 +1,15 @@
- print ("Have trouble making a choice? Don't worry we can help you out.")
+import random
+
+print ("Have trouble making a choice? Don't worry we can help you out.")
+print("\n")
+
+numberofitems=int(input("How many options do you have?"))
+print("OK, enter your choices now one by one and hit enter")
+
+listItems = []
+
+for x in range (numberofitems):
+  listItems.append(input(""))
+
+chosenItem = random.choice(listItems)
+print("I think that you should go with: " + chosenItem)
