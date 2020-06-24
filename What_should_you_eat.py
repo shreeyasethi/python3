3990959cd8d2e87586486959518eec5d2e1e5573
shreeyasethi <67246350+shreeyasethi@users.noreply.github.com>
2020-06-24 12:35:06 -0400

    Update What_should_you_eat.py

--- What_should_you_eat.py
+++ What_should_you_eat.py
@@ -5,24 +5,19 @@ hunger = input("Are you hungry on a scale of 1 to 5? "
 + "(1 - not very, 5 - super) :")
 hunger = int(hunger) # convert to a number
 
-print("If it's tuesday, "\
-+ "I don't care how hungry you are. " \
-+ "You should eat some tacos!")
+if dayofweek.lower() == "tuesday":
+  print("I don't care how hungry you are. " \
+  + "You should eat some tacos!")
+elif dayofweek.lower() =="friday":
+  print("I don't care how hungry you are. " \
+  + "You should definitely be eating pizza...")
 
-print("If it's friday, "\
-+ "I don't care how hungry you are. " \
-+ "You should definitely be eating pizza...")
-
-print("Otherwise,")
-
-print("If your hunger level is less than 3, " \
-+ "you should try a salad.")
-
-print("If your hunger level is between 3 and 5, " \
-+ "you're hungry enough to eat whatever.")
-
-print("If your hunger level is 5, " \
-+ "I see a giant burrito in your future.")
-
-print("Otherwise, why are you taking a quiz? " \
-+ "GO EAT EVERYTHING.")
+elif hunger < 3:
+  print("you should try a salad.")
+elif hunger == 3:
+  print("you're hungry enough to eat whatever.")
+elif hunger > 5:
+  print("I see a giant burrito in your future.")
+else: 
+  print("Otherwise, why are you taking a quiz? " \
+  + "GO EAT EVERYTHING.")
